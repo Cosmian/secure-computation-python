3f9b6957ddc0bf21a7cac416e167acf1d5f382e1
Franco <francois.colas@cosmian.com>
2022-02-24 14:26:06 +0100

    update: crypto tests

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,20 +1,39 @@
-from cosmian_client_sgx.crypto.helper import seal, unseal, random_symkey
+from cosmian_client_sgx.crypto.helper import *
+
+from keys import *
 
 
-TEST_X25519_PUBKEY = bytes.fromhex("1f80306ddf75ee31bc8f71f29c93768bc6eaba2c1f67bcd7f179ca26d4361331")
-TEST_X25519_PRIVKEY = bytes.fromhex("deb832a69e996898c835b9779c3a98cd3ba0b437a6aba94dacc33692154a815c")
+def test_encrypt():
+    message: bytes = b"Hello World!"
+
+    assert (len(CP_SYMKEY) == len(DP1_SYMKEY) ==
+            len(DP2_SYMKEY) == len(RC_SYMKEY) == 32)
+
+    ciphertext: bytes = encrypt(message, CP_SYMKEY)
+    cleartext: bytes = decrypt(ciphertext, CP_SYMKEY)
+    assert message == cleartext
 
 
 def test_sealing():
     message: bytes = b"Hello World!"
-    ciphertext: bytes = seal(message, TEST_X25519_PUBKEY)
-    cleartext: bytes = unseal(ciphertext, TEST_X25519_PRIVKEY)
 
+    assert (len(CP_PUBKEY) == len(DP1_PUBKEY) ==
+            len(DP2_PUBKEY) == len(RC_PUBKEY) == 32)
+    assert (len(CP_PRIVKEY) == len(DP1_PRIVKEY) ==
+            len(DP2_PRIVKEY) == len(RC_PRIVKEY) == 32)
+
+    ciphertext: bytes = seal(message, CP_PUBKEY)
+    cleartext: bytes = unseal(ciphertext, CP_PRIVKEY)
     assert message == cleartext
 
-    symkey: bytes = random_symkey()
-    print(symkey.hex())
-    print(len(symkey))
-    c: bytes = seal(symkey, TEST_X25519_PUBKEY)
-    print(c.hex())
-    print(len(c))
+    ciphertext: bytes = seal(message, DP1_PUBKEY)
+    cleartext: bytes = unseal(ciphertext, DP1_PRIVKEY)
+    assert message == cleartext
+
+    ciphertext: bytes = seal(message, DP2_PUBKEY)
+    cleartext: bytes = unseal(ciphertext, DP2_PRIVKEY)
+    assert message == cleartext
+
+    ciphertext: bytes = seal(message, RC_PUBKEY)
+    cleartext: bytes = unseal(ciphertext, RC_PRIVKEY)
+    assert message == cleartext
