030d92c2edb7a34d33a1d266731895aed04eaae5
Franco <francois.colas@cosmian.com>
2022-08-18 15:33:17 +0200

    bump version to 0.4.2b3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cosmian_secure_computation_client",
-    version="0.4.2b2",
+    version="0.4.2b3",
     url="https://cosmian.com",
     license="MIT",
     author="Cosmian Tech",
